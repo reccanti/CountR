7e16260ec789d531aa0dec51480f8b41fe7d7072
Ben Wilcox <reccanti@gmail.com>
2017-02-23 12:07:22 -0500

    Create initial counter app
    Creates the initial counter app where everything is
    stored in the ViewController

--- CountR/ViewController.swift
+++ CountR/ViewController.swift
@@ -10,8 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var CounterLabel: UILabel!
+    @IBOutlet weak var IncrementButton: UIButton!
+    @IBOutlet weak var DecrementButton: UIButton!
+    @IBOutlet weak var ClearButton: UIButton!
+    
+    var count:Int = 0 {
+        didSet {
+            if (count > 999) {
+                count = 999
+            } else if (count < 0) {
+                count = 0
+            }
+        }
+    };
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        CounterLabel.text = "\(count)"
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +36,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    /**
+     * Handle when a Count button is
+     * is clicked
+     */
+    @IBAction func countButtonClicked(_ sender: UIButton) {
+        if (sender.tag == 100) {
+            count = 0
+        } else if (sender.tag == 110) {
+            count += 1
+        } else if (sender.tag == 120) {
+            count -= 1
+        }
+        CounterLabel.text = "\(count)"
+    }
 
 }
 
